56ce6c9002984c92056659c47f8cbc673976f9a5
zhaoyadong <zhaoyadong@baidu.com>
2025-04-19 09:39:02 +0800

    更新theme state

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,7 +1,12 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-export const useThemeStore = create(
+export interface ThemeState {
+    theme: "light" | "dark";
+    setTheme: (newTheme: "light" | "dark") => void;
+}
+
+export const useThemeStore = create<ThemeState>()(
     persist(
         (set) => ({
             theme: "light",
